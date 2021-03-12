1d6acab8a6ba8be6685dbda13dd364e1ce1b098a
Tim <bossenti@posteo.de>
2021-03-12 18:53:04 +0100

    add heroku deploy

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
+import os
+
 from flask import Flask
 
 application = Flask(__name__)
 
+
 @application.route('/')
 def hello_world():
-    return 'Hello, GitHub Actions World!'
+    name = os.environ.get('NAME', "")
+    return f'Hello {name}, thats GitHub Actions World!'
 
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    application.run(threaded=True, port=5000)
