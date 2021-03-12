8e024a7905a499e8747a56873c45f24d273ecdf8
Tim <bossenti@posteo.de>
2021-03-12 19:07:52 +0100

    check env handling

--- application.py
+++ application.py
@@ -7,9 +7,13 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    name = os.environ.get('NAME', "")
+    name = os.getenv('NAME', "")
     return f'Hello {name}, thats GitHub Actions World!'
 
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    application.run(threaded=True, port=5000)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
